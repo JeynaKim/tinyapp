f15d4337ab21d38f1ea21c73199139ade6c904a6
Jeyna Kim <cocomimusic@gmail.com>
2022-05-17 16:59:11 -0400

    Updated for url_shows.ejs info

--- express_server.js
+++ express_server.js
@@ -40,15 +40,22 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  let longURL = req.body.longURL;
+  urlDatabase[shortURL] = longURL;
+  res.redirect("/urls");
+});
+
 app.get("/u/:shortURL", (req, res) => {
   const longURL = urlDatabase[req.params.shortURL];
   res.redirect(longURL);
 });
 
-app.post("/urls/:shortURL/delete"), (req, res) => {
+app.post("/urls/:shortURL/delete", (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect("/urls");
-}
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
