f559ae2c8095145908fcec68ec95b574be9201cd
Jeyna Kim <jeyna.kimm@gmail.com>
2022-05-20 03:58:46 +0000

    database updated

---
+++ data/database.js
@@ -0,0 +1,36 @@
+// urlDatabase information 
+const urlDatabase = {
+  b6UTxQ: {
+    longURL: "https://www.tsn.ca",
+    userID: "aJ48lW",
+  },
+  i3BoGr: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+  sgq3y6: {
+    longURL: "https://www.google.ca",
+    userID: "aJ48lW",
+  },
+};
+
+// users information
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+  usID: {
+    id: "usID",
+    email: "1",
+    password: "$2a$10$oe6JxGvGmu4oyStUYct3g.de9svhTMLVNAI5SLhrdsQwefjuuL7Hy",
+  },
+};
+
+module.exports = {urlDatabase, users};
